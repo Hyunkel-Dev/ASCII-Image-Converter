6bcaf9ededa35ebe3532e6026aa6806195434cec
Hyunkel <maxencebonamy@gmail.com>
2022-02-27 21:42:50 +0100

    27/02/2022

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # the width (in characters) of the output image, the height is then calculated with the image ratio
-WIDTH = 150
+WIDTH = 100
 
 # a predefined string that ranks the characters from lightest to darkest
 ASCII = " .\'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
 
 # the name of the image you want to convert
-IMAGE_NAME = "example.png"
+IMAGE_NAME = "example.jpg"
